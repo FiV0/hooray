1f6162c809a548e0644f403c4be57c95081c9a3a
FiVo <finn.volkel@gmail.com>
2022-08-16 21:58:41 +0200

    dean helpers update-in and get-in

--- src/hooray/util/dean.clj
+++ src/hooray/util/dean.clj
@@ -5,18 +5,18 @@
 
 (defn update-in
   "Like `clojure.core/update-in` but also works for interval trees"
-  ([m ks f & args]
-   (let [up (fn up [m ks f args]
-              (let [[k & ks] ks]
-                (if (instance? IntervalMap m)
-                  (let [old (some-> (find m k) val)]
-                    (if ks
-                      (assoc m k (up old ks f args))
-                      (assoc m k (apply f old args))))
-                  (if ks
-                    (assoc m k (up (get m k) ks f args))
-                    (assoc m k (apply f (get m k) args))))))]
-     (up m ks f args))))
+  [m ks f & args]
+  (let [up (fn up [m ks f args]
+             (let [[k & ks] ks]
+               (if (instance? IntervalMap m)
+                 (let [old (some-> (find m k) val)]
+                   (if ks
+                     (assoc m k (up old ks f args))
+                     (assoc m k (apply f old args))))
+                 (if ks
+                   (assoc m k (up (get m k) ks f args))
+                   (assoc m k (apply f (get m k) args))))))]
+    (up m ks f args)))
 
 (comment
   (require '[com.dean.interval-tree.core :as dean])
@@ -30,4 +30,37 @@
       val
       type))
 
-(defn get-in)
+(defn get-in
+  "Like `clojure.core/get-in` but also works for interval trees"
+  ([m ks]
+   (get-in m ks nil))
+  ([m ks not-found]
+   (loop [sentinel (Object.)
+          m m
+          ks (seq ks)]
+     (if ks
+       (if (instance? IntervalMap m)
+         (if-let [m (some-> (find m (first ks)) val)]
+           (recur sentinel m (next ks))
+           not-found)
+         (let [m (get m (first ks) sentinel)]
+           (if (identical? sentinel m)
+             not-found
+             (recur sentinel m (next ks)))))
+       m))))
+
+(comment
+  (-> (update-in (dean/interval-map) [[1 2] :b] (fnil conj []) 1)
+      (get-in [[1 2] :b]))
+
+  (-> {}
+      (assoc :b (dean/interval-map {[1 2] [1]}))
+      (get-in [:b [1 2]]))
+
+  (-> {}
+      (assoc :b (dean/interval-map {[1 2] [1]}))
+      (get-in [[1 2] :b]))
+
+  )
+
+;; TODO assoc-in
