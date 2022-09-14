831e04b87227dd3979ca7afd617069dd8e54a5aa
FiVo <finn.volkel@gmail.com>
2022-09-14 18:41:30 +0200

    logging with unilog

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,9 @@
 (ns user
   (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
             [clojure.tools.namespace.repl :as repl]
-            [lambdaisland.classpath.watch-deps :as watch-deps]))
+            [lambdaisland.classpath.watch-deps :as watch-deps]
+            [unilog.config :as ul]))
 
 (defn watch-deps!
   []
@@ -10,6 +12,15 @@
 (defn go []
   (watch-deps!))
 
+
+(defn better-logging! []
+  (ul/start-logging! {:external  false
+                      :console   "%d{yyyy-MM-dd HH:mm:ss.SSS} %highlight(%-5level) %logger{36} - %msg%n"
+                      :level     :info
+                      :overrides {"hooray"                                     :debug}}))
+
+(better-logging!)
+
 (comment
   (repl/set-refresh-dirs (io/file "src") (io/file "dev"))
   (repl/refresh)
@@ -17,4 +28,5 @@
 
   (watch-deps!)
 
+  (log/info :foo "foo")
   )
