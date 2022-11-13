a44d4be5d73f61b8d83ab6f962ccf628fbdd0e37
FiV0 <finn.volkel@gmail.com>
2022-11-13 17:25:25 +0100

    comment out everything in xt testing ns

--- src/hooray/testing/xt.clj
+++ src/hooray/testing/xt.clj
@@ -3,50 +3,56 @@
             [xtdb.query :as query]
             [clojure.edn :as edn]))
 
-(defn wrap-in-puts [data]
-  (map #(vector ::xt/put %) data))
+(comment
+  (defn wrap-in-puts [data]
+    (map #(vector ::xt/put %) data))
 
-(def data (->> (edn/read-string (slurp "resources/transactions.edn"))
-               (map #(clojure.set/rename-keys % {:db/id :xt/id}))
-               (wrap-in-puts)))
+  (def data (->> (edn/read-string (slurp "resources/transactions.edn"))
+                 (map #(clojure.set/rename-keys % {:db/id :xt/id}))
+                 (wrap-in-puts)))
 
-(def node (xt/start-node {}))
-(.close node)
 
-(xt/submit-tx node data)
+  (def node (xt/start-node {}))
+  (.close node)
 
-(defn db [] (xt/db node))
+  (xt/submit-tx node data)
 
-(xt/q (db)
-      '{:find [?name]
-        :where
-        [[?t :track/name "For Those About To Rock (We Salute You)"]
-         [?t :track/album ?album]
-         [?album :album/artist ?artist]
-         [?artist :artist/name ?name]]})
+  (defn db [] (xt/db node))
+
+
+  (xt/q (db)
+        '{:find [?name]
+          :where
+          [[?t :track/name "For Those About To Rock (We Salute You)"]
+           [?t :track/album ?album]
+           [?album :album/artist ?artist]
+           [?artist :artist/name ?name]]})
 
 ;;;;;;;;;;;;;;;;;
-;; triangle query
+  ;; triangle query
 ;;;;;;;;;;;;;;;;;
 
-(require '[hooray.graph-gen :as g-gen])
+  (require '[hooray.graph-gen :as g-gen])
 
-(def random-graph (clojure.edn/read-string (slurp "resources/random-graph-100-0.3.edn")))
-(def random-independents (clojure.edn/read-string (slurp "resources/random-independents-graph-100-0.3.edn")))
+  (def random-graph (clojure.edn/read-string (slurp "resources/random-graph-100-0.3.edn")))
+  (def random-independents (clojure.edn/read-string (slurp "resources/random-independents-graph-100-0.3.edn")))
 
-(defn graph->ops [g]
-  (->> (g-gen/edge-list->adj-list g)
-       (map (fn [[from adj-list]] {:xt/id from :g/to (into #{} adj-list)}))))
+  (defn graph->ops [g]
+    (->> (g-gen/edge-list->adj-list g)
+         (map (fn [[from adj-list]] {:xt/id from :g/to (into #{} adj-list)}))))
 
-(def random-graph-data (graph->ops random-graph))
-(def random-independents-graph-data (graph->ops random-independents))
+  (def random-graph-data (graph->ops random-graph))
+  (def random-independents-graph-data (graph->ops random-independents))
 
-(xt/submit-tx node (wrap-in-puts random-graph-data))
-(xt/submit-tx node (wrap-in-puts random-independents-graph-data))
 
-(def triangle-query '{:find [?a ?b ?c]
-                      :where [[?a :g/to ?b]
-                              [?a :g/to ?c]
-                              [?b :g/to ?c]]})
+  (xt/submit-tx node (wrap-in-puts random-graph-data))
+  (xt/submit-tx node (wrap-in-puts random-independents-graph-data))
 
-(time (count (xt/q (db) triangle-query)))
+
+  (def triangle-query '{:find [?a ?b ?c]
+                        :where [[?a :g/to ?b]
+                                [?a :g/to ?c]
+                                [?b :g/to ?c]]})
+
+
+  (time (count (xt/q (db) triangle-query))))
